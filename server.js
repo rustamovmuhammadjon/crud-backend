faf0efccd04236ccb5e1491acb8104966165f912
Muhammadjon <mrustamov0109@gmail.com>
2026-03-08 01:10:05 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(cors());
-app.options("/", cors()); // preflight
+// 1️⃣ CORS middleware
+app.use(cors()); // default: hamma domenlarga ruxsat beradi
+
 app.use(express.json());
 
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+// test route
+app.get("/", (req, res) => res.send("API ishlayapti 🚀"));
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
