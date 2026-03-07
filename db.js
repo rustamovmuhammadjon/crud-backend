a10402155661109d845d5e1d395ec0e814c29538
Muhammadjon <mrustamov0109@gmail.com>
2026-03-08 00:48:32 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: "postgresql://neondb_owner:npg_9sYt6UcBamuh@ep-curly-dew-a8pxtnmw-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
