493cc669c1ff93a7b7c288558229a395060a155f
Muhammadjon <mrustamov0109@gmail.com>
2026-03-10 22:59:49 +0500

    some changes

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const { Pool } = require("pg");
+const DATABASE_URL = 'postgresql://neondb_owner:npg_9sYt6UcBamuh@ep-curly-dew-a8pxtnmw-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require'
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
